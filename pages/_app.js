1b6072f36146099f9c8467c033c2a160657e9892
Abolfazl Ahmadi <a.ahmadi@firststar.ir>
2019-11-16 19:45:25 +0330

    added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
   
 import React from 'react'
 import App from 'next/app'
-// import Head from "next/head";
+import Head from "next/head";
 
 import { appWithTranslation } from '../i18n'
 
@@ -9,12 +9,21 @@ class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props
     return (
+      <>
+      <Head>
 
+                <link
+                  rel="shortcut icon"
+                  type="image/x-icon"
+                  href="public/favicon.ico"
+                />
+      </Head>
+           
       <Component {...pageProps} />
+      </>
 
     )
   }
 }
 
 export default appWithTranslation(MyApp)
-// export default MyApp
