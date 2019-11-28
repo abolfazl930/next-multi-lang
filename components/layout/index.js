5fac742485783f457810fa0b9a44c72f070ac65c
abolfazl ahmadi <a.abolfazl930@gmail.com>
2019-11-29 00:22:37 +0330

    added direction

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,36 +1,50 @@
 import React from "react";
 import { i18n, withTranslation, Link } from "../../i18n";
 
-const Layout = ({ children, t }) => {
+const Layout = props => {
   return (
     <>
-      <div>
-        <button type="button" onClick={() => i18n.changeLanguage("en")}>
-          en
-        </button>
-        <button type="button" onClick={() => i18n.changeLanguage("fa")}>
-          fa
-        </button>
+      <div className="main">
+        <div className="change-lang-btn">
+          <button type="button" onClick={() => props.i18n.changeLanguage("en")}>
+            en
+          </button>
+          <button type="button" onClick={() => props.i18n.changeLanguage("fa")}>
+            fa
+          </button>
+        </div>
+
         <ul>
           <li>
             <Link href="/">
-              <a>{t("item1")}</a>
+              <a>{props.t("item1")}</a>
             </Link>
           </li>
           <li>
             <Link href="/second-page">
-              <a>{t("item2")}</a>
+              <a>{props.t("item2")}</a>
             </Link>
           </li>
         </ul>
-        {children}
+        {props.children}
       </div>
+      <style jsx>{`
+        .main {
+          direction: ${props.i18n.language === "en" ? "ltr" : "rtl"};
+        }
+        .change-lang-btn {
+          display: flex;
+          justify-content: center;
+          direction: ltr !important;
+        }
+      `}</style>
     </>
   );
 };
 
-Layout.getInitialProps = async () => ({
-  namespacesRequired: ["nav"]
+Layout.getInitialProps = async ({ req }) => ({
+  namespacesRequired: ["nav"],
+  currentLanguage: req ? req.language : i18n.language
 });
 
 export default withTranslation("nav")(Layout);
